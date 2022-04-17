7056b73ea4736ec0c3aefbca7d99517e588d6a50
VanNgoc1102 <ngocnv.uet@gmail.com>
2022-04-17 22:56:45 +0700

    code password ngau nhien co do dai cho trc

---
+++ password_random.py
@@ -0,0 +1,27 @@
+#project :random password generator
+#5
+#password: 12-jk
+import string
+import random
+letters=string.ascii_letters
+number=string.digits
+punctuation=string.punctuation
+def password_generator(length):
+    printable=f'{letters}{number}{punctuation}'
+    printable=list(printable)
+    random.shuffle(printable)
+    random_password=random.choices(printable,k=length)
+    random_password= ''.join(random_password)
+    return(random_password)
+def get_password_length():
+    password_lenght=input("How long do you want your password :")
+    return int(password_lenght)
+def main():
+    password_length=get_password_length()
+    password=password_generator(password_length)
+    
+    print(password)
+
+
+if __name__ =="__main__":
+    main()
